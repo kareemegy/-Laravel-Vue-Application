33aaba97bbbbcd96126bfbc2166ae9af94cf1131
= <kokekarim@yahoo.com>
2020-05-17 18:00:09 +0200

    add fontawesom

--- public/js/app.js
+++ public/js/app.js
@@ -49858,8 +49858,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\larastart\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\larastart\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\Laravel-Vue-Application\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\Laravel-Vue-Application\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
